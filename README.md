5d9a47f85b417d6678b4eeae00f4438858cc102f
filipposerio <filippo.serio@gmail.com>
2015-06-19 11:13:42 +0200

    primo commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# cadutisiculiana1518
+
+a [Sails](http://sailsjs.org) application
---
+++ api/controllers/CadutiController.js
@@ -0,0 +1,11 @@
+/**
+ * CadutiController
+ *
+ * @description :: Server-side logic for managing cadutis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
